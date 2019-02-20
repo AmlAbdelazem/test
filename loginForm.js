2f1dee63a74f3c541021b7ac81c5a9f5c26462b3
AmlAbdelazem <aml.abdelazem@outlook.com>
2019-02-20 17:17:00 +0200

    intial commit

---
+++ loginForm.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',function(e){
+    var reg = document.getElementById("reg");
+    
+    reg.addEventListener('click', function(){
+        window.location.href = "register.html";
+    })
+})
